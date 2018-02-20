b8d8abcdc3b035cfb56bf67a5ece7c4ab3d032ba
chai2 <chaitulittle@gmail.com>
2018-02-19 23:06:12 -0800

    updated regions

--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -1,66 +1,28 @@
 class RegionsController < ApplicationController
 
-	def index
-	    @regions = Region.all
-		json_response(@regions)
+  before_action :get_regions
 
-  
-  # parsed_json = ActiveSupport::JSON.decode("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson")
-  # parsed_json["results"].each do |longUrl, convertedUrl|
-  		
-  # 		site = Site.find_by_long_url(longUrl)
-  # 		site.short_url = convertedUrl["shortUrl"]
-  # 		site.save
+	def index
+    begin 
+      param! :name, String
+      param! :days, Integer, default: 30
+      param! :count, Integer, default: 10
+
+      @regions = @regions.county_name(params["name"]) if params[:name].present?      
+      @regions = @regions.past_days(params[:days]) if params[:days].present?
+
+      query_data = Region.process_data(@regions, params["count"])
+      json_response(query_data)
+
+    rescue InvalidParameterError => e
+      json_response("Invalid parameter values \nCount: #{params[:count]} & Days: #{params[:days]} \n#{e}")
+    end
 	end
 
+  private
+
+  def get_regions
+    @regions = Region.largest_magnitude
+  end
+
 end
-
-# {
-# 	"type":"Feature",
-# 	"properties":
-# 		{ "mag":1.48,
-# 			"place":"30km NE of Oakhurst, CA",
-# 			"time":1518651199030,
-# 			"updated":1518653350680,
-# 			"tz":-480,
-# 			"url":"https://earthquake.usgs.gov/earthquakes/eventpage/nc72969576",
-# 			"detail":"https://earthquake.usgs.gov/earthquakes/feed/v1.0/detail/nc72969576.geojson",
-# 			"felt":null,
-# 			"cdi":null,
-# 			"mmi":null,
-# 			"alert":null,
-# 			"status":"reviewed",
-# 			"tsunami":0,
-# 			"sig":34,
-# 			"net":"nc",
-# 			"code":"72969576",
-# 			"ids":",nc72969576,",
-# 			"sources":",nc,",
-# 			"types":",focal-mechanism,geoserve,nearby-cities,origin,phase-data,scitech-link,",
-# 			"nst":26,
-# 			"dmin":0.1662,
-# 			"rms":0.1,
-# 			"gap":131,
-# 			"magType":"md",
-# 			"type":"earthquake",
-# 			"title":"M 1.5 - 30km NE of Oakhurst, CA"
-# 		},
-# 	"geometry":{
-# 		"type":"Point",
-# 		"coordinates":[-119.3761667,37.4836667,9.76]},
-# 		"id":"nc72969576"
-# }
-
-# region:
-
-# 	name: properties["place"].split(" of ")[1]
-# 	created_at: properties["time"]
-# 	updated_at: properties["updated"]
-# 	timezone: properties["tz"]
-# 	mag: properties["mag"]
-
-
-
-
-
-
--- app/models/region.rb
+++ app/models/region.rb
@@ -1,2 +1,42 @@
 class Region < ApplicationRecord
+	scope :largest_magnitude, -> { order('magnitude desc') }
+	scope :days, ->(days) { past_days(days) }
+	scope :county_name, -> (name) {where("name like ?", "%#{name}%")}
+
+	def self.past_days(days)
+		where(occured_at: (days.to_i).days.ago..Date.today)
+	end
+
+	def self.process_data(regions, limit)
+		regions_grouped = regions.group(:name)
+		regions_count = regions_grouped.count
+		regions_total_magnitude = regions_grouped.sum("magnitude")
+
+		data = Array.new
+		regions_grouped.each do |r|
+			total_magnitude = regions_total_magnitude[r["name"]].round(2)
+			total_earthquake_count = regions_count[r["name"]]
+			region_hash = Hash.new
+			region_hash[total_magnitude] = {:name => r["name"], :count => total_earthquake_count }
+			data << region_hash
+		end
+
+		sorted_by_magnitude = (data.sort_by {|region| region.keys[0]}).reverse
+
+		data.clear
+		sorted_by_magnitude.each do |region|
+			region_info = Hash.new
+			region_info["name"] = region.first.last[:name]
+			region_info["total_magnitude"] = region.first.first
+			region_info["earthquake_count"] = region.first.last[:count]
+			data << region_info
+		end
+
+		return data[0..limit-1]
+	end
+
+	def energy
+		(11.8 + 1.5 * self.magnitude)
+	end
+
 end
